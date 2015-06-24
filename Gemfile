3c8196966df5f3b5073582ba21ca0443a02c3e2a
rastasheep <rastasheep3@gmail.com>
2015-06-24 21:03:48 +0200

    Initial dump

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "https://rubygems.org"
+
+gem "bundler", ">= 1.8.4"
+
+
+gem "middleman", "~> 4.0.0.beta.2"
+gem "middleman-core"
+gem "middleman-cli"
+gem "middleman-sprockets"
+gem "middleman-minify-html"
+#gem "middleman-imageoptim"
+
+gem "scss_lint", :require => false
+
+source "https://rails-assets.org" do
+  gem "rails-assets-angular"
+end
---
+++ config.rb
@@ -0,0 +1,45 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+helpers do
+  def svg(file_name)
+    partial "inline-assets/#{file_name}"
+  end
+end
+
+configure :development do
+#  activate :asset_hash
+end
+
+configure :build do
+  activate :asset_hash
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
+#  activate :imageoptim
+end
